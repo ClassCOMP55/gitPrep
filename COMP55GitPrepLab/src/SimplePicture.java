a2110229c1d37e50594d281acf9782895c82b40b
afsoony <67535264+afsoony@users.noreply.github.com>
2022-09-26 12:48:11 -0700

    Initial commit

---
+++ COMP55GitPrepLab/src/SimplePicture.java
@@ -0,0 +1,19 @@
+//comment one
+import acm.graphics.GImage;
+import acm.graphics.GLabel;
+import acm.program.GraphicsProgram;
+
+public class SimplePicture extends GraphicsProgram {
+	public static int SIZE = 700;
+	
+	public void init() {
+		setSize(SIZE, SIZE);
+	}
+	
+	public void run() {
+		GImage robot = new GImage("robot.jpg", 200, 100);
+		add(robot);
+		GLabel label = new GLabel("How I feel when using github hackslash", 200, 400);
+		add(label);
+	}
+}
